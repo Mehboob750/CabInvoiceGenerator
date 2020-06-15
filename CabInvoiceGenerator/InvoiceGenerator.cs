2aa572388a60a6a36dfbaa377c18bfdf6519ea67
Mehboob750 <ms568203@gmail.com>
2020-06-15 15:51:10 +0530

    Added: Given distance & time to Invoice Generator it gives Minimum Fare

--- CabInvoiceGenerator/InvoiceGenerator.cs
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -8,10 +8,14 @@ namespace CabInvoiceGenerator
     {
         private static readonly int MinimumCostPerKiloMeter = 10;
         private static readonly int CostPerTime = 1;
+        private static readonly double MinimumFare = 5;
 
         public double CalculateFare(double distance, int time)
         {
-            return distance *  MinimumCostPerKiloMeter + time * CostPerTime;
+            double totalFare = distance * MinimumCostPerKiloMeter + time * CostPerTime;
+            if (totalFare < MinimumFare)
+                return MinimumFare;
+            return totalFare;
         }
     }
 }
--- NUnitTestCabInvoiceGenerator/UnitTest1.cs
+++ NUnitTestCabInvoiceGenerator/UnitTest1.cs
@@ -19,5 +19,15 @@ namespace NUnitTestCabInvoiceGenerator
             double fare = invoiceGenerator.CalculateFare(distance,time);
             Assert.AreEqual(25, fare);
         }
+
+        [Test]
+        public void GivenLessDistanceAndTime_ShouldReturnMinimumFare()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            double distance = 0.1;
+            int time = 1;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            Assert.AreEqual(5, fare);
+        }
     }
 }
